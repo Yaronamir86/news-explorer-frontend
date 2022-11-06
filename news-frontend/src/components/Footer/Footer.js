1e3949e21b642a0a1da98e8c6d1d11869abcaf50
yaron amir <yaronamir86@gmail.com>
2022-11-06 11:08:01 +0200

    finishing page

--- news-frontend/src/components/Footer/Footer.js
+++ news-frontend/src/components/Footer/Footer.js
@@ -1,10 +1,32 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
 import "./Footer.css";
+import Facebook from "../../images/facebook.svg";
+import Github from "../../images/github.svg";
 
-const Footer = () => {
+const footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer-container">
+      <div className="footer__copyrights">
+        © 2022 Supersite, Powered by Yaron amir. All rights reserved.
+      </div>
+      <div className="footer__socials">
+        <ul className="footer__list">
+          <li className="footer__list-item">
+            <a href="#">Home</a>
+          </li>
+          <li className="footer__list-item"><a href="https://practicum.com">Practicum
+        </a></li>
+          <li className="footer__list-item"><a href="https://github.com">
+         <img src={Github} alt='Github-logo'/>
+        </a></li>
+          <li className="footer__list-item"><a href="https://facebook.com">
+         <img src={Facebook} alt='facebook-logo'/>
+        </a></li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer;
+export default footer;
